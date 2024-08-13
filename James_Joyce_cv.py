36f4b6c3e9b4dba3f2d71fc991dff2fd5f712c26
jimjamjoyce <139064989+jimjamjoyce@users.noreply.github.com>
2024-08-13 22:52:16 +0100

    Update James_Joyce_cv.py

--- James_Joyce_cv.py
+++ James_Joyce_cv.py
@@ -366,5 +366,6 @@ with tab_contact:
     PHONE: +44 7535 358 691
     </div>''', unsafe_allow_html=True)
     ""
-    st.page_link("http://www.linkedin.com/in/jj-james-joyce", label="LinkedIn")
+    st.page_link("http://www.linkedin.com/in/jj-james-joyce", label="🔗 LinkedIn")
+    ""
     st.link_button("Get in touch", "mailto:jimjoywork@live.com", type = "primary")

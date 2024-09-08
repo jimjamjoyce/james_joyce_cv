c5b5cec1a683bb8b56bd0a2b9a80dcd7ae72406e
jimjamjoyce <139064989+jimjamjoyce@users.noreply.github.com>
2024-09-08 22:07:05 +0100

    Update James_Joyce_cv.py

--- James_Joyce_cv.py
+++ James_Joyce_cv.py
@@ -176,10 +176,10 @@ with tab_intro:
 #########################################
 with tab_cv:
 
-    cv = os.path.join(downloadfile_path, 'James_Joyce_CV_24.pdf')
+    cv = os.path.join(downloadfile_path, 'James_Joyce_CV_24.2.pdf.zip')
     st.download_button(label='Export CV',
                        data=cv,
-                       file_name='James_Joyce_CV_24.pdf')
+                       file_name='James_Joyce_CV_24.2.pdf.zip')
 
     col1, col2, = st.columns([1.7,5],gap='small',vertical_alignment='top')
     with col1:

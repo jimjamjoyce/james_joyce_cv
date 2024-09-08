64782d7fd631091675a39dae550b929d4253a053
jimjamjoyce <139064989+jimjamjoyce@users.noreply.github.com>
2024-09-08 22:23:59 +0100

    Update James_Joyce_cv.py

--- James_Joyce_cv.py
+++ James_Joyce_cv.py
@@ -177,17 +177,10 @@ with tab_intro:
 with tab_cv:
 
     cv = os.path.join(downloadfile_path, 'James_Joyce_CV_24.2.pdf')
-    # st.download_button(label='Export CV',
-    #                    data=cv,
-    #                    file_name='James_Joyce_CV_24.2.pdf',
-    #                    mime='application/octet-stream')
-    
-    with open("yourpdf.pdf", "rb") as file:
-      btn=st.download_button(
-      label="Export CV",
-      data=cv,
-      file_name="James_Joyce_CV_24.2.pdf",
-      mime="application/octet-stream")
+    st.download_button(label='Export CV',
+                       data=cv,
+                       file_name='James_Joyce_CV_24.2.pdf',
+                       mime='application/octet-stream')
 
     col1, col2, = st.columns([1.7,5],gap='small',vertical_alignment='top')
     with col1:
